b3ad3d00ebad875612df9fec86f2b30ff30b095e
Adrian <aabeijoncarbajo@danielcastelao.org>
2023-11-27 09:07:20 +0100

    first commit

---
+++ src/Trabajador.java
@@ -0,0 +1,43 @@
+public class Trabajador {
+    String nombre;
+    double sueldo;
+
+    //
+    //
+    //
+    //
+    //
+
+
+    public Trabajador() {
+    }
+
+    public Trabajador(String nombre, double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Trabajador{" +
+                "nombre='" + nombre + '\'' +
+                ", sueldo=" + sueldo +
+                '}';
+    }
+}
